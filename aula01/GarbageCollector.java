0e9432e07f401b2202d7dcf0d2823926402b5089
Francisco Chaves <francisco.jschaves@gmail.com>
2015-08-02 23:33:26 -0300

    Conhecendo Garbage Collector
    Quando utilizar uma String o java esta direcionando o objeto para um
    valor e nao modificando o seu valor, ao mudar o conteúdo da string o
    garbage collector (lixeiro) vem e limpar o antigo objeto nao utilizado,
    eliminando o uso de mémoria desnecesaria.

---
+++ aula01/GarbageCollector.java
@@ -0,0 +1,17 @@
+package com.junio.aula01;
+
+public class GarbageCollector {
+
+	public static void main(String[] args) {
+
+		// Variável Primitiva
+		int x = 7;
+		x = 9;
+
+		// Variável de Referência
+		String y = "XTI";
+		y = "xti.com.br";
+		y = null;
+	}
+
+}
