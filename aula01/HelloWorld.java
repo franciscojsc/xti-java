8a243f149c3f05b26c7fea50c4134f70c8952daf
Francisco Chaves <francisco.jschaves@gmail.com>
2015-08-02 23:26:33 -0300

    Hello World

---
+++ aula01/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.junio.aula01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("oi mundo!");
+		System.out.println("Java SE");
+		/*
+		 * Sequencia de escape
+		 * 
+		 * \t \" \\ \n
+		 */
+	}
+
+}
