e5630c6b37e574ed2763b0fc67a72589b46fece7
Francisco Chaves <francisco.jschaves@gmail.com>
2015-08-02 23:37:45 -0300

    Constante
    Quando precisar utilizar uma váriavel que o valor não poderá se
    modificado, se faz necessario o uso de uma constante, utlizando a
    palavra reservada final antes do tipo de váriavel, quando se trata de
    constante escreve o nome da váriavel em caixa alta.

---
+++ aula01/Constante.java
@@ -0,0 +1,15 @@
+package com.junio.aula01;
+
+public class Constante {
+
+	public static void main(String[] args) {
+
+		int populacaoBrasileira = 203429773;
+		final double PI = 3.14159265358979323846;
+		final char SEXO_MASCULINO = 'F';
+		final char SEXO_FEMININO = 'M';
+
+		System.out.println(PI);
+	}
+
+}
