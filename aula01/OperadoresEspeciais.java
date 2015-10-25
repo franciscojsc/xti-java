84584139b9df43b15893a84794c61078259a79e4
Francisco Chaves <francisco.jschaves@gmail.com>
2015-10-25 18:56:56 -0200

    Operadores especiais

---
+++ aula01/OperadoresEspeciais.java
@@ -0,0 +1,24 @@
+package com.junio.aulaXTI;
+
+public class OperadoresEspeciais {
+
+	public static void main(String[] args) {
+
+		int idade = 26;
+		
+		//operador tern�rio
+		String x = (idade >= 18) ? "Maior de Idade" : "Menor de Idade";
+
+		String sexo = "M", pais = "pais";
+		// utilizando o operador de
+		// separacao(virgular ,)para declara duas ou
+		// mais variaveis na mesma linha
+
+		System.out.println(x);
+		
+		
+		
+
+	}
+
+}
