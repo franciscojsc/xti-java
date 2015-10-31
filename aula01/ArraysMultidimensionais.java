40aa49f710e05f72cae43e6baaead20181d6783f
Francisco Chaves <francisco.jschaves@gmail.com>
2015-10-30 23:54:04 -0200

    ArraysMultidimensionais

---
+++ aula01/ArraysMultidimensionais.java
@@ -0,0 +1,16 @@
+package com.junio.aulaXTI;
+
+public class ArraysMultidimensionais {
+
+	public static void main(String[] args) {
+
+		String[] uma = { "Francisco", "Sandra" };
+		String[][] duas = { { "Francisco", "M", "SE" }, { "Sandra", "F", "BH" } };
+
+		System.out.println(duas[0][0]);
+		System.out.println(duas[1][0]);
+		System.out.println(duas.length);
+
+	}
+
+}
