2a9f172f1298aa2124ad0440f8a670fc6f460e6d
Francisco Chaves <francisco.jschaves@gmail.com>
2015-10-31 23:55:54 -0200

    update ArraysMultidimensionais

--- aula01/ArraysMultidimensionais.java
+++ aula01/ArraysMultidimensionais.java
@@ -6,7 +6,9 @@ public class ArraysMultidimensionais {
 
 		String[] uma = { "Francisco", "Sandra" };
 		String[][] duas = { { "Francisco", "M", "SE" }, { "Sandra", "F", "BH" } };
-
+		
+		String[][][] tres = new String[10][10][10];
+		
 		System.out.println(duas[0][0]);
 		System.out.println(duas[1][0]);
 		System.out.println(duas.length);
