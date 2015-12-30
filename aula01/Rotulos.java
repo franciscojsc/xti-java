85960e8079531f46035ff70fe3f88a8672e518af
Francisco Chaves <francisco.jschaves@gmail.com>
2015-12-30 14:05:26 -0200

    Rotulos
    Utilizando rotulo para para um loop

---
+++ aula01/Rotulos.java
@@ -0,0 +1,19 @@
+package com.junio.aulaXTI;
+
+public class Rotulos {
+
+	public static void main(String[] args) {
+		boolean[][] matrix = { { false, true, false, false, false }, { false, false, false, false, false } };
+
+		busca: for (int a = 0; a < matrix.length; a++) {
+			System.out.print("A ");
+			for (int b = 0; b < matrix[a].length; b++) {
+				if (matrix[a][b]) {
+					System.out.print("TRUE");
+					break busca;
+				}
+			}
+		}
+	}
+
+}
