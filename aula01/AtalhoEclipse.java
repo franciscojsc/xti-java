e16686b3a9503197b52875483407dd81af69e305
Francisco Chaves <francisco.jschaves@gmail.com>
2015-12-30 14:04:02 -0200

    Atalho Eclipse

---
+++ aula01/AtalhoEclipse.java
@@ -0,0 +1,29 @@
+package com.junio.xti.logica;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AtalhoEclipse {
+
+	@SuppressWarnings({ "unused", "resource" })
+	public AtalhoEclipse() {
+		Scanner leitor = new Scanner(System.in);
+		// ctrl + shif + O --IMPORTA TODAS CLASS QUE ESTA PRECISANDO
+
+		leitor.nextLine();
+		int[] array = { 1, 2, 3, 4 };
+		for (int i = 0; i < array.length; i++) {
+
+		}
+
+		ArrayList<String> lista = new ArrayList<>();
+		for (String string : lista) {
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
