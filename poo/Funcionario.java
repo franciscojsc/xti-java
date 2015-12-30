c3cd60f0b93cccf3cefd16a7f577215286d42fac
Francisco Chaves <francisco.jschaves@gmail.com>
2015-12-30 14:14:11 -0200

    Utilizando os Modificado de acesso

---
+++ poo/Funcionario.java
@@ -0,0 +1,29 @@
+package com.junio.xti.poo;
+
+public class Funcionario {
+	
+	private String nome;
+	private boolean ativo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+	
+
+	
+
+}
---
+++ poo/FuncionarioPacote.java
@@ -0,0 +1,12 @@
+package com.junio.xti.poo;
+
+public class FuncionarioPacote {
+
+	public static void main(String[] args) {
+
+		Funcionario f = new Funcionario();
+		f.getNome();
+		f.setNome("junio");
+	}
+
+}
