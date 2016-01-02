4ca8cac4478b1e1d029af97e9b8126d80508b70e
Francisco Chaves <francisco.jschaves@gmail.com>
2016-01-02 13:08:31 -0200

    Utilizando o Relacionamento TEM-UM Composição
    exemplo o Carro tem um motor,tem um tanque,tem um ar-codicionado e etc.
    Criar a classe para intergar ao Classe Carro,se houve necessidade para
    uso.

--- workspace/xti/src/com/junio/xti/poo/Carro.java
+++ workspace/xti/src/com/junio/xti/poo/Carro.java
@@ -5,15 +5,21 @@ public class Carro {
 	String modelo;
 	int velocidadeMaxima;
 	double segundoZeroACem;
+	Motor motor;
 
 	public Carro() {
 
 	}
 
 	public Carro(String modelo, int velocidadeMaxima, double segundoZeroACem) {
+		this(modelo, velocidadeMaxima, segundoZeroACem, null);
+	}
+
+	public Carro(String modelo, int velocidadeMaxima, double segundoZeroACem, Motor motor) {
 		this.modelo = modelo;
 		this.velocidadeMaxima = velocidadeMaxima;
 		this.segundoZeroACem = segundoZeroACem;
+		this.motor = motor;
 	}
 
 }
--- workspace/xti/src/com/junio/xti/poo/CarroTest.java
+++ workspace/xti/src/com/junio/xti/poo/CarroTest.java
@@ -8,10 +8,21 @@ public class CarroTest {
 		ferrari.modelo = "Ferrari Enzo";
 		ferrari.velocidadeMaxima = 349;
 		ferrari.segundoZeroACem = 3.2;
-		
+		Motor v12 = new Motor();
+		v12.tipo = "v12";
+		v12.potencia = 660;
+		ferrari.motor = v12;// adicionar o motor ao carro
+
 		Carro Koenigsegg = new Carro("Koenigsegg CCXR", 430, 3.1);
+		Motor v8 = new Motor("v8", 101);
+		Koenigsegg.motor = v8;
+
+		System.out.println(Koenigsegg.motor.potencia);
 		System.out.println(Koenigsegg.modelo);
-		
+
+		Carro bugatti = new Carro("Bugatti", 430, 2.2, new Motor("w16", 1200));
+		System.out.println(bugatti.motor.potencia);
+
 	}
 
 }
