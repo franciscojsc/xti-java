28237e9bcbd4785e9fbd9aa16e4fb8e6e30bd320
Francisco Chaves <francisco.jschaves@gmail.com>
2016-06-20 14:32:31 -0300

    Tratamento de erros com try,catch e finally
    O tratamento de erro, é utilizado para aplicação não para em caso de
    alguma excessão ocorra durante a execução.

---
+++ workspace/xti/src/com/junio/xti/erros/DividePorZero.java
@@ -0,0 +1,38 @@
+package com.junio.xti.erros;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DividePorZero {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		boolean continua = false;
+
+		do {
+			try {
+
+				System.out.println("Numero: ");
+				int a = teclado.nextInt();
+				System.out.println("Divisor: ");
+				int b = teclado.nextInt();
+
+				System.out.println(a / b);
+
+				continua = true;
+
+			} catch (InputMismatchException e1) {
+				System.err.println("Numeros devem ser inteiro");
+				teclado.nextLine();// descarta a entrada errada e libera novamente para o usuario
+									 
+			} catch (ArithmeticException e2) {
+				System.err.println("Divisor dever ser diferente de Zero");
+			}finally {
+				System.out.println("Finally executado...");//sempre é executado
+			}
+
+		} while (!continua);
+
+	}
+
+}
