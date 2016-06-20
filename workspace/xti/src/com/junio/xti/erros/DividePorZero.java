2c4a24598fe97cf6ef959e0a84ec1c7bab853fb1
Francisco Chaves <francisco.jschaves@gmail.com>
2016-06-20 15:22:21 -0300

    A clasula throws serve para indicar que aquele metodo poderá lancar uma exceção.

--- workspace/xti/src/com/junio/xti/erros/DividePorZero.java
+++ workspace/xti/src/com/junio/xti/erros/DividePorZero.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 
 public class DividePorZero {
 
+	public static void dividir(Scanner teclado) 
+	throws InputMismatchException, ArithmeticException{
+		System.out.println("Numero: ");
+		int a = teclado.nextInt();
+		System.out.println("Divisor: ");
+		int b = teclado.nextInt();
+
+		System.out.println(a / b);
+	}
+
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		boolean continua = false;
@@ -21,15 +31,15 @@ public class DividePorZero {
 
 				continua = true;
 
-			} catch (ArithmeticException |InputMismatchException e1 ) {
+			} catch (ArithmeticException | InputMismatchException e1) {
 				System.err.println("Numeros invalido");
 				e1.printStackTrace();
-				//System.out.println(e1.getStackTrace());
-				//System.out.println(e1.getMessage());
+				// System.out.println(e1.getStackTrace());
+				// System.out.println(e1.getMessage());
 				teclado.nextLine();// descarta a entrada errada e libera novamente para o usuario
-									 
-			}finally {
-				System.out.println("Finally executado...");//sempre é executado
+
+			} finally {
+				System.out.println("Finally executado...");// sempre é executado
 			}
 
 		} while (!continua);
