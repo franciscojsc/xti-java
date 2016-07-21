edabc62dcce94151dc50bc4d9fa9338dc0c7d33e
Francisco Chaves <francisco.jschaves@gmail.com>
2016-07-21 12:29:04 -0300

    Threads com a interface Runnable
    Para que threads possa ser utilizada em classe que possui heranca,
    existe a interface Runnable que permitir a criacao de threads.

---
+++ workspace/xti/src/com/junio/xti/threads/PingPongRunnable.java
@@ -0,0 +1,38 @@
+package com.junio.xti.threads;
+
+public class PingPongRunnable implements Runnable {
+
+	String palavra;
+	long tempo;
+
+	public PingPongRunnable(String palavra, long tempo) {
+		this.palavra = palavra;
+		this.tempo = tempo;
+	}
+
+	public void run() {
+
+		try {
+			for (int i = 0; i < 10; i++) {
+				System.out.println(palavra);
+				Thread.sleep(tempo);
+			}
+		} catch (InterruptedException e) {
+			return;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Runnable ping = new PingPongRunnable("ping", 1500);
+		Runnable pong = new PingPongRunnable("pong", 2500);
+
+		new Thread(ping, "ping").start();
+		new Thread(pong, "pong").start();
+		
+
+		System.out.println("Ver Thread");
+
+	}
+
+}
