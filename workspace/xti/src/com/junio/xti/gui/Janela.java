5ef831f9c1b714d39a69894a107de3c81c6c360f
Francisco Chaves <francisco.jschaves@gmail.com>
2016-07-30 15:56:35 -0300

    Interface Gráfica com Swing
    utlizando herança para a classe Jframe

--- workspace/xti/src/com/junio/xti/gui/Janela.java
+++ workspace/xti/src/com/junio/xti/gui/Janela.java
@@ -3,18 +3,16 @@ package com.junio.xti.gui;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Janela{
+public class Janela extends JFrame{
 
 	public Janela(){
-		JFrame frame = new JFrame();//janela
+		super("Minha Janela");
 		
 		JButton botao = new JButton("Clique");//botao 
-		frame.getContentPane().add(botao);//container
-		
-		frame.setTitle("Minha Janela");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//quando o usuario clica em fecha a janela a aplicação sera encerrada
-		frame.setSize(300, 300);//tamanho da janela
-		frame.setVisible(true);//visibilidade
+		getContentPane().add(botao);//container com um botão
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//quando o usuario clica em fecha a janela a aplicação sera encerrada
+		setSize(300, 300);//tamanho da janela
+		setVisible(true);//visibilidade
 	}
 	
 	public static void main(String[] args) {
