194afe3f5498524a4f0c7564ed0b58dfd8b90266
Francisco Chaves <francisco.jschaves@gmail.com>
2016-07-30 15:33:05 -0300

    Utlizando a Classe Math

---
+++ workspace/xti/src/com/junio/xti/java/MathTest.java
@@ -0,0 +1,18 @@
+package com.junio.xti.java;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.E);
+		System.out.println(Math.PI);
+		System.out.println(Math.sin(10));//seno
+		System.out.println(Math.cos(10));//cosseno
+		System.out.println(Math.tan(45));//tangente
+		System.out.println(Math.pow(4, 3));//potência
+		System.out.println(Math.sqrt(16));//raiz quadrada
+		System.out.println(Math.cbrt(16));//raiz cúbica
+		System.out.println(Math.hypot(6, 8));//hypotenusa
+	}
+
+}
