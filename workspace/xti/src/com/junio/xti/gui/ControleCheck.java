a65886a4c5f7a1a7696c787a557f0d9217c460a5
Francisco Chaves <francisco.jschaves@gmail.com>
2016-08-13 09:29:50 -0300

    Utilizando checkBox, para alterar o texto para bold ou italic

---
+++ workspace/xti/src/com/junio/xti/gui/ControleCheck.java
@@ -0,0 +1,61 @@
+package com.junio.xti.gui;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class ControleCheck extends JFrame {
+
+	JTextField texto;
+	JCheckBox bold = new JCheckBox("Bold");
+
+	JCheckBox italic = new JCheckBox("Italic");
+
+	public ControleCheck() {
+		super();
+		bold.addItemListener(new CheckListener());
+		italic.addItemListener(new CheckListener());
+		texto = new JTextField("Veja a Fonte Mudar", 12);
+		texto.setFont(new Font("Serif", Font.PLAIN, 26));
+
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.add(texto);
+		c.add(bold);
+		c.add(italic);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 300);
+		setVisible(true);
+
+	}
+
+	class CheckListener implements ItemListener {
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			if (bold.isSelected() && italic.isSelected()) {
+
+				texto.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 26));
+			} else if (bold.isSelected()) {
+				texto.setFont(new Font("Serif", Font.BOLD, 26));
+			} else if (italic.isSelected()) {
+				texto.setFont(new Font("Serif", Font.ITALIC, 26));
+			} else {
+				texto.setFont(new Font("Serif", Font.PLAIN, 26));
+
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		new ControleCheck();
+	}
+}
