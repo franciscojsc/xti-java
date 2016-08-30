eca6cc4cc6871ae8a0d38ada972b14b3018efd56
Francisco Chaves <francisco.jschaves@gmail.com>
2016-08-30 15:44:34 -0300

    Utilizando o Runtime e seus métodos

---
+++ workspace/xti/src/com/junio/xti/refinado/Garbage.java
@@ -0,0 +1,37 @@
+package com.junio.xti.refinado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garbage {
+
+	public static long carregarMemoria(){
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i <100_000; i++) {
+			list.add(i);
+		}
+		return Runtime.getRuntime().freeMemory();
+	}
+	
+	
+	public static void main(String[] args) {
+
+		Runtime rt = Runtime.getRuntime();
+		int MB = 1_048_576;//total de bytes em 1MB
+		double total = rt.maxMemory()/MB;
+		
+		double inicio = total - (carregarMemoria()/MB);
+		
+		System.out.println(total);
+		System.out.println(inicio);
+		
+		rt.runFinalization();
+		rt.gc();
+		
+		double fim = total - (rt.freeMemory()/MB);
+	
+		System.out.println("Início: "+inicio+" Fim: "+ fim);
+		
+	}
+
+}
