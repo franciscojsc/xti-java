f13a5d9f4c743485af790958f0e0dadd3cbfbeed
Francisco Chaves <francisco.jschaves@gmail.com>
2016-08-30 21:26:51 -0300

    Recursividade

---
+++ workspace/xti/src/com/junio/xti/refinado/Recursividade.java
@@ -0,0 +1,51 @@
+package com.junio.xti.refinado;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Recursividade {
+
+	public static int soma(int x){
+		if(x== 0){
+			return x;
+		}else{
+			System.out.println(x);
+			return x + soma(x-1);
+		}
+	}
+	
+	/*potencia (3,4)  = 3 * 3 * 3 * 3*/
+	public static int potencia(int x,int e){
+		if(e == 1){
+			return x;
+		}else{
+			return x*potencia( x,e-1); 
+		}
+	}
+	
+	public static void listar(Path path){
+		if(Files.isRegularFile(path)){
+			System.out.println(path.toAbsolutePath());
+		}else{
+			System.err.println(path.toAbsolutePath());
+			try( DirectoryStream<Path> stream = Files.newDirectoryStream(path)){
+				for (Path p : stream) {
+					//System.out.println(p.toAbsolutePath());
+					listar(p);
+				}
+			} catch (Exception e) {
+			}
+		}
+		 
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println(soma(10));
+		System.out.println(potencia(3,4));
+		listar(Paths.get("C:/Users"));
+	}
+
+}
