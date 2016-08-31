451223ba1e857e63b0e9539733328e54e961fa85
Francisco Chaves <francisco.jschaves@gmail.com>
2016-08-31 20:50:13 -0300

    Utilizando o JavaDoc com eclipse para gerar documentação

--- workspace/xti/src/com/junio/xti/threads/Ponte.java
+++ workspace/xti/src/com/junio/xti/threads/Ponte.java
@@ -1,8 +1,41 @@
 package com.junio.xti.threads;
 
+/**
+ * Uma <code>Ponte<code> representa o elo de 
+ * ligação entre objetos Produtores e Consumidores
+ * de informações. Os <i>Produtores<i> utillizam as
+ * Pontes para gravar informações e compartilhar 
+ * essas informações com os Consumidores que lêem
+ * esses dados da Ponte para fazer o procesamento.
+ * 
+ * 
+ * @author Francisco Junio
+ * @Ricardo Lopes Costa
+ * @version 1.3
+ * @since 1.0
+ * 
+ * @see PonteNaoSincronizada
+ * @see PonteSicronizada
+ *
+ */
 public interface Ponte {
 
+	/**
+	 * 	Armazenar o valor informado na ponte. Geralmente
+	 * será chamado pelas calsses Produtora de dados.
+	 * 
+	 * @param valor
+	 * @throws InterruptedException
+	 */
 	public void set(int valor) throws InterruptedException;
+	
+	/**
+	 * Recuperar a informação armazenada na Ponte. Esse 
+	 * método será chamado pelas Consumidoras de dados.
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
 	public int  get() throws InterruptedException;
 	
 }
