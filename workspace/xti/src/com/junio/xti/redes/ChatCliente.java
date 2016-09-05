bccf023e60da33e344b4a6dc6a65f05d9c2a87e0
Francisco Chaves <francisco.jschaves@gmail.com>
2016-09-05 14:02:41 -0300

    update chat
    Adicionando uma área para exibir a mensagem aos cliente, que esta sendo
    enviadas para o servidor.

--- workspace/xti/src/com/junio/xti/redes/ChatCliente.java
+++ workspace/xti/src/com/junio/xti/redes/ChatCliente.java
@@ -7,10 +7,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatCliente extends JFrame {
@@ -19,9 +23,24 @@ public class ChatCliente extends JFrame {
 	PrintWriter escritor;
 	Socket socket;
 	String nome;
-	
+	JTextArea textoRecebido;
+	Scanner leitor;
+
+	private class EscutaServidor implements Runnable {
+		@Override
+		public void run() {
+			try {
+				String texto;
+				while ((texto = leitor.nextLine()) != null) {
+					textoRecebido.append(texto + "\n");
+				}
+
+			} catch (Exception e) {	}
+		}
+	}
+
 	public ChatCliente(String nome) {
-		super("chat : "+nome);
+		super("chat : " + nome);
 		this.nome = nome;
 
 		Font fonte = new Font("Serif", Font.PLAIN, 26);
@@ -30,42 +49,51 @@ public class ChatCliente extends JFrame {
 		JButton botao = new JButton("enviar");
 		botao.setFont(fonte);
 		botao.addActionListener(new EnviarListener());
-		
+
 		Container envio = new JPanel();
 		envio.setLayout(new BorderLayout());
 		envio.add(BorderLayout.CENTER, textoParaEnviar);
 		envio.add(BorderLayout.EAST, botao);
+
+		textoRecebido = new JTextArea();
+		textoRecebido.setFont(fonte);
+		JScrollPane scrollPane = new JScrollPane(textoRecebido);
+
 		getContentPane().add(BorderLayout.SOUTH, envio);
-		
+		getContentPane().add(BorderLayout.CENTER, scrollPane);
+
 		configurarRede();
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500, 90);
+		setSize(500, 500);
 		setVisible(true);
 
 	}
-	
-	private class EnviarListener implements ActionListener{
+
+	private class EnviarListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
-			escritor.println(nome+" : "+textoParaEnviar.getText());
+
+			escritor.println(nome + " : " + textoParaEnviar.getText());
 			escritor.flush();
 			textoParaEnviar.setText("");
 			textoParaEnviar.requestFocus();
 		}
-		
+
 	}
 
-	private void configurarRede(){
-		try{
-			
-		 socket = new Socket("127.0.0.1",5000);
-		 escritor = new PrintWriter(socket.getOutputStream());
-			
-		}catch(Exception e){	}
-		
+	private void configurarRede() {
+		try {
+
+			socket = new Socket("127.0.0.1", 5000);
+			escritor = new PrintWriter(socket.getOutputStream());
+			leitor = new Scanner(socket.getInputStream());
+			new Thread(new EscutaServidor()).start();
+
+		} catch (Exception e) {
+		}
+
 	}
 
 	public static void main(String[] args) {
--- workspace/xti/src/com/junio/xti/redes/ChatServer.java
+++ workspace/xti/src/com/junio/xti/redes/ChatServer.java
@@ -1,14 +1,20 @@
 package com.junio.xti.redes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ChatServer {
 
 	Scanner leitor;
-
+	
+	
+	List<PrintWriter > escritores = new ArrayList<>();
+	
 	public ChatServer() {
 		ServerSocket server;
 		try {
@@ -16,6 +22,8 @@ public class ChatServer {
 			while (true) {
 				Socket socket = server.accept();
 				new Thread(new EscutaCliente(socket)).start();
+				PrintWriter p = new PrintWriter(socket.getOutputStream());
+				escritores.add(p);
 			}
 
 			// leitor = new Scanner (s.getInputStream());
@@ -25,6 +33,18 @@ public class ChatServer {
 		}
 	}
 
+	private void encaminharParaTodos(String texto){
+		for (PrintWriter printWriter : escritores) {
+			try{
+				printWriter.println(texto);
+			printWriter.flush();
+			}catch(Exception e){
+				
+			}
+			
+		}
+	}
+	
 	private class EscutaCliente implements Runnable {
 
 		Scanner leitor;
@@ -43,9 +63,8 @@ public class ChatServer {
 			try {
 				String texto;
 				while ((texto = leitor.nextLine()) != null) {
-
 					System.out.println(texto);
-
+					encaminharParaTodos(texto);;
 				}
 			} catch (Exception e) {
 
